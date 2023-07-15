8b3284b0f280409f0ac127c556e84749779afd9c
Basim Aljazaeri <30289772+ShiroYaksha90@users.noreply.github.com>
2023-07-15 15:12:06 +0300

    Implement required changes

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ApplicationRecord
-  belongs_to :users, class_name: 'User'
-  belongs_to :posts, class_name: 'Post'
+  belongs_to :author, class_name: 'User'
+  belongs_to :post, foreign_key: 'post_id'
 
   def update_comments_counter
     post.increment(:comments_counter)
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-  belongs_to :user, foreign_key: 'author_id'
+  belongs_to :author, class_name: 'User' , foreign_key: 'author_id'
   has_many :likes
   has_many :comments, foreign_key: 'post_id'
 
@@ -7,7 +7,7 @@ class Post < ApplicationRecord
 
   # Updates the posts counter for a user
   def update_post_counter
-    user.increment!(:posts_counter)
+    author.increment!(:posts_counter)
   end
 
   # Returns the 5 most recent comments for a given post
