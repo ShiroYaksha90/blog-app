ef8b6a43adb37d708dc864b0e7f4c83ec220dbe5
Basim Aljazaeri <30289772+ShiroYaksha90@users.noreply.github.com>
2023-07-15 21:33:23 +0300

    Add validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,9 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   has_many :likes
   has_many :comments, foreign_key: 'post_id'
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, numericality: { greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { greater_than_or_equal_to: 0 }
 
   after_save :update_post_counter
 
