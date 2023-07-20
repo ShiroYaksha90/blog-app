f199fe1b65484f3aff6e3ce271329869b77727a8
Basim Aljazaeri <30289772+ShiroYaksha90@users.noreply.github.com>
2023-07-20 13:02:15 +0300

    Fix linters

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe 'Users', type: :request do
   describe 'GET /index' do
     describe 'users#index' do
-      it 'returns http success for index' do 
+      it 'returns http success for index' do
         get users_path
         expect(response).to have_http_status(:success)
         expect(response).to render_template(:index)
