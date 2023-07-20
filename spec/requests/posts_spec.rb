f76c2333cce528be85ca8dc2ba39a35e97c6ee6b
Basim Aljazaeri <30289772+ShiroYaksha90@users.noreply.github.com>
2023-07-20 13:00:41 +0300

    Implement posts_spec testing

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -2,6 +2,21 @@ require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
   describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+    describe 'posts#index' do
+      it 'returns http success for post index' do
+        get '/users/index/posts'
+        expect(response).to have_http_status(:success)
+        expect(response).to render_template('posts/index')
+        expect(response.body).to include 'Hello, the posts will be listed here ...'
+      end
+    end
+    describe 'posts#show' do
+      it 'returns http success for post show' do
+        get '/users/index/posts/index'
+        expect(response).to have_http_status(:success)
+        expect(response).to render_template('posts/show')
+        expect(response.body).to include 'Post details'
+      end
+    end
   end
 end
