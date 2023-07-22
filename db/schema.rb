9d4f2c80dc81eefd1274b1366ccb8a31ec8682d0
Basim Aljazaeri <30289772+ShiroYaksha90@users.noreply.github.com>
2023-07-22 17:58:46 +0300

    Add default values

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_14_215157) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_22_144326) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -39,8 +39,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_14_215157) do
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "comments_counter"
-    t.integer "likes_counter"
+    t.integer "comments_counter", default: 0
+    t.integer "likes_counter", default: 0
     t.index ["author_id"], name: "index_posts_on_author_id"
   end
 
@@ -48,12 +48,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_14_215157) do
     t.string "name"
     t.string "photo"
     t.text "bio"
-    t.integer "posts_counter"
+    t.integer "posts_counter", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "comments", "posts", column: "post_id"
+  add_foreign_key "comments", "posts"
   add_foreign_key "comments", "users", column: "author_id"
   add_foreign_key "likes", "posts"
   add_foreign_key "likes", "users", column: "author_id"
