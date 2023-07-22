ee0927595360e26afe28691a6b52cd64826b8edb
Basim Aljazaeri <30289772+ShiroYaksha90@users.noreply.github.com>
2023-07-22 16:09:22 +0300

    Add new & create methods

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,9 +3,31 @@ class PostsController < ApplicationController
     @user = User.find(params[:user_id])
     @posts = Post.find(params[:user_id])
   end
+  
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.author = current_user
+
+    if @post.save
+      redirect_to user_post_path(current_user, @post), notice: 'Post was successfully created.'
+    else
+      render :new, alert: 'Post was not created.'
+    end
+  end
 
   def show
     @user = User.find(params[:user_id])
     @post = @user.posts.find(params[:id])
   end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
+  
 end
