e764b96ac1b2af3d7ace5b83cc8791bd9b495019
Basim Aljazaeri <30289772+ShiroYaksha90@users.noreply.github.com>
2023-07-28 04:25:28 +0300

    Update post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,13 +5,8 @@ class Post < ApplicationRecord
   validates :title, presence: true, length: { maximum: 250 }
   validates :comments_counter, numericality: { greater_than_or_equal_to: 0 }
   validates :likes_counter, numericality: { greater_than_or_equal_to: 0 }
-
   after_save :update_post_counter
 
-  # Updates the posts counter for a user
-  def update_post_counter
-    author.increment!(:posts_counter)
-  end
 
   # Returns the 5 most recent comments for a given post
   def recent_comments
