fec316c939fe0dd604fbecd101fc4e788d225204
Basim Aljazaeri <30289772+ShiroYaksha90@users.noreply.github.com>
2023-07-28 04:21:30 +0300

    Update ability.rb file

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,7 +5,17 @@ class Ability
 
   def initialize(user)
     # Define abilities for the user here. For example:
-    #
+    return unless user.present?
+
+    can :read, :all
+    can :manage, User, id: user.id
+    can :manage, Post, author_id: user.id
+    can :create, Comment
+    can :destroy, Comment, author_id: user.id
+
+    return unless user.admin?
+
+    can :manage, :all
     #   return unless user.present?
     #   can :read, :all
     #   return unless user.admin?
