8cbde9f50114520db652bee8163ccb5e3947f726
Basim Aljazaeri <30289772+ShiroYaksha90@users.noreply.github.com>
2023-07-28 04:25:06 +0300

    Update comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,6 +3,6 @@ class Comment < ApplicationRecord
   belongs_to :post, foreign_key: 'post_id'
 
   def update_comments_counter
-    post.increment(:comments_counter)
+    post.increment!(:comments_counter)
   end
 end
