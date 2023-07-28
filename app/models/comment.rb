b751ed8ba56f4febea466c762c364d73de908c2e
Riyaz Ahmed <r-ahmed@riysaim.com>
2023-07-28 09:24:37 +0530

    updated comments.rb

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,13 @@ class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post, foreign_key: 'post_id'
 
+  after_save :update_comments_counter
+  before_destroy :decrement_comment
+
+  def decrement_comment
+     post.decrement!(:comments_counter)
+  end
+
   def update_comments_counter
     post.increment!(:comments_counter)
   end
