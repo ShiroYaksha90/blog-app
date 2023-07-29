e6792a284084051089c91e722c1f3c5ebf1a25ff
Riyaz Ahmed <r-ahmed@riysaim.com>
2023-07-29 23:40:34 +0530

    added controller for users api

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,14 @@
+class Api::V1::UsersController < ApplicationController
+    skip_before_action :authenticate_user!
+    protect_from_forgery with: :null_session
+  
+    def index
+      @users = User.all
+      render json: @users
+    end
+  
+    def show
+      @user = User.find_by(id: params[:id])
+      render json: @user
+    end
+  end
