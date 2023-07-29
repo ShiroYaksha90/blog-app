4836db12f82701af232c752643545619efbc4468
Basim Aljazaeri <30289772+ShiroYaksha90@users.noreply.github.com>
2023-07-29 21:09:35 +0300

    Fix linter issues

--- app/serializers/api/v1/user_serializer.rb
+++ app/serializers/api/v1/user_serializer.rb
@@ -1,5 +1,5 @@
 class Api::V1::UserSerializer < ActiveModel::Serializer
-  attributes :id,:name,:bio,:posts_counter
+  attributes :id, :name, :bio, :posts_counter
   has_many :posts
   has_many :comments
   has_many :likes
